186ed3c30ccf719fac0ece78c8d8847eaf78371a
FelipeCasari <110491590+FelipeCasari@users.noreply.github.com>
2024-08-07 18:19:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const caixaPerguntas = document.querySelector('.caixa-perguntas');
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
 const caixaResultado = document.querySelector('.caixa-resultado');
 
-const perguntas = [//serve para abrir çosta de perguntas
-{
-    enunciado:"Pergunta 1",
-    alternativas:[
-        "Alternativa 1",
-        "Alternativa 2"
-    ]
-}
+const perguntas = [//serve para abrir lista de perguntas
+    {//abre o objetivo das peguntas
+        enunciado: "Pergunta 1",
+        alternativas: ["Alternativa 1", "Alternativa 2"]
+    },
+    {//abre o objetivo das peguntas
+        enunciado: "Pergunta 2",
+        alternativas: ["Alternativa 1", "Alternativa 2"]
+    }
 ]
