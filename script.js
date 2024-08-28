6abc059b77eea981615dee554153813d2a58d334
FelipeCasari <110491590+FelipeCasari@users.noreply.github.com>
2024-08-28 18:29:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,13 +3,31 @@ const caixaPerguntas = document.querySelector('.caixa-perguntas');
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
 const caixaResultado = document.querySelector('.caixa-resultado');
 
-const perguntas = [//serve para abrir lista de perguntas
-    {//abre o objetivo das peguntas
-        enunciado: "Pergunta 1",
-        alternativas: ["Alternativa 1", "Alternativa 2"]
+const perguntas = [ //serve para abrir lista de perguntas
+    {//abre o objeto das perguntas
+        enunciado: "Em sua opinião, a I.A. poderá ser mais bem aproveitada futuramente, ou será degradada?",
+        alternativas: ["Será bem aproveitada","Irá degradar-se"]
     },
-    {//abre o objetivo das peguntas
-        enunciado: "Pergunta 2",
-        alternativas: ["Alternativa 1", "Alternativa 2"]
+    {
+        enunciado: "Você pensa que a I.A. desenvolverá pessoas ou fará elas regredirem?",
+        alternativas: ["Desenvolverá","Fará elas regredirem"]
+    },
+    {
+        enunciado: "Você se vê usando a I.A. com grande frequência futuramente?",
+        alternativas: ["Sim, me vejo","Não, não me vejo"]
     }
 ]
+
+let posicao = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[posicao]
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas(){
+
+}
+
+mostraPergunta();
