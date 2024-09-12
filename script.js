d1aaf707e9fb89fbe0161622b00774089a9ab317
FelipeCasari <110491590+FelipeCasari@users.noreply.github.com>
2024-09-12 16:48:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,6 +15,18 @@ const perguntas = [ //serve para abrir lista de perguntas
     {
         enunciado: "Você se vê usando a I.A. com grande frequência futuramente?",
         alternativas: ["Sim, me vejo","Não, não me vejo"]
+    },
+    {
+        enunciado: "Existe maior probabilidade da I.A. ser universalmente utilizada ou utilizada apenas para processos específicos?",
+        alternativas: ["Maior probabilidade de ser universalizada","Maior probabilidade de ser utilizada em processos específicos"]
+    },
+    {
+        enunciado: "Você utiliza, utilizou ou utilizaria produções feitas inteiramente por I.A.?",
+        alternativas: ["Utilizei, mas não utilizei mais","Utilizei e utilizaria","Não utilizei e não utilizaria","Não utilizei, mas utilizaria"]
+    },
+    {
+        enunciado: "Você tem conhecimento da abrangência onde a I.A. pode ser utilizada e/ou já teve conato com isso?",
+        alternativas: ["Sim","Não"]
     }
 ]
 
@@ -22,12 +34,12 @@ let posicao = 0;
 let perguntaAtual;
 
 function mostraPergunta(){
-    perguntaAtual = perguntas[posicao]
+    perguntaAtual = perguntas[posicao];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     mostraAlternativas();
 }
 function mostraAlternativas(){
-
+    
 }
 
 mostraPergunta();
